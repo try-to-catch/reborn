f52236cadd84707857797c0e8b58e0e8128eea81
Kostya <koooooostaaa@gmail.com>
2023-03-13 22:10:59 +0200

    made a chat page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { createApp } from 'vue'
+import './../css/app.css'
 
-const app = createApp({});
-
-app.mount('#app');
+// const app = createApp({});
+//
+// app.mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                main: {
+                    '900': '#1C1E21',
+                    '800': '#222427',
+                    '700': '#26292D',
+                    '500': '#2C2F33',
+                    '300': '#35383D',
+                },
+                'primary': '#31363D'
+            },
+        },
     },
     plugins: [],
 }
