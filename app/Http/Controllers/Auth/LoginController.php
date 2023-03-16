518d2d6b869f8b79c470a045c8145d8124e98767
Kostya <koooooostaaa@gmail.com>
2023-03-16 21:35:14 +0200

    Add auth controllers, form requests and user resource

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginFormRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginFormRequest $request): JsonResponse|UserResource
+    {
+
+        if (Auth::attempt($request->validated())) {
+            $request->session()->regenerate();
+
+            return new UserResource(auth()->user());
+        }
+
+        return response()->json(['errors' => ['email' => ['Invalid email address or password.'], 'password' => []]], 401);
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterFormRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterFormRequest $request): JsonResponse|UserResource
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+        $data['username'] = ('@' . $data['username']);
+
+        try {
+            $user = User::query()->create($data);
+        } catch (Exception $e) {
+            return response()->json($e, 401);
+        }
+
+        return new UserResource($user);
+    }
+}
