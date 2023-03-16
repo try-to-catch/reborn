7331bc39338affd1494b67df6006d47a8da1af75
Kostya <koooooostaaa@gmail.com>
2023-03-16 21:14:25 +0200

    add auth routes

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', \App\Http\Controllers\Auth\LoginController::class);
+Route::post('/sign-up', \App\Http\Controllers\Auth\RegisterController::class);
+
 Route::view('/{page}', 'app')->where('page', '.*');
