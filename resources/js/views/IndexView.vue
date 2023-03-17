84d0878d4f1203daeaa5226960d2fea0091388d4
Kostya <koooooostaaa@gmail.com>
2023-03-17 18:51:48 +0200

    create index page

---
+++ resources/js/views/IndexView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Index View
+        <div class="space-x-2 text-blue-700 mt-10">
+            <router-link :to="{ name: 'login'}">Login</router-link>
+            <router-link :to="{ name: 'chats.index'}">Chats</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexView"
+}
+</script>
+
+<style scoped>
+
+</style>
