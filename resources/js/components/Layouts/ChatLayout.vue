61fd9134b6cc35f5e12098620fec40fb6d742bcb
Kostya <koooooostaaa@gmail.com>
2023-03-17 21:26:22 +0200

    split the component into parts

---
+++ resources/js/components/Layouts/ChatLayout.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="bg-main-900 text-white flex flex-col h-screen">
+        <header class="basis-[36px] flex content-end">
+            <div class="text-lg mt-1.5 ml-2.5">reborn</div>
+        </header>
+        <main class="flex bg-main-900 grow">
+            <div class="bg-main-900 basis-[75px]"></div>
+            <div class="bg-main-700 2xl:basis-[315px] basis-[265px] rounded-tl-2xl flex flex-col z-10">
+                <div class="grow flex flex-col items-center mt-6">
+                    <div class="w-full px-5">
+                        <input type="text"
+                               class="bg-main-500 w-full h-8 rounded-[5px] px-2 focus:outline focus:outline-2 focus:outline-slate-600"
+                               placeholder="Search">
+                    </div>
+                    <div class="pt-8 flex justify-between w-full px-5 items-center cursor-pointer">
+                        <div>
+                            <font-awesome-icon icon="fa-solid fa-user-group" class="mr-3"/>
+                            Friends
+                        </div>
+                    </div>
+                    <div class="pt-8 flex justify-between w-full px-5 items-center text-sm">
+                        <div>PRIVATE MESSAGES</div>
+                        <div class="text-2xl hover:cursor-pointer">+</div>
+                    </div>
+                    <div class="w-full px-5 pt-5 flex flex-col space-y-4">
+                        <div
+                            class="w-full relative flex items-center after:hidden hover:after:block hover:after:content-['✕'] after:absolute after:top-1 after:right-1 after:text-gray-200 after:cursor-pointer">
+
+                            <div class="bg-white w-8 h-8 rounded-full bg-gray-200"></div>
+                            <div class="ml-4 text-gray-200 font-medium">Schaste</div>
+                        </div>
+                        <div
+                            class="w-full relative flex items-center after:hidden hover:after:block hover:after:content-['✕'] after:absolute after:top-1 after:right-1 after:text-gray-200 after:cursor-pointer">
+
+                            <div class="bg-white w-8 h-8 rounded-full bg-gray-200"></div>
+                            <div class="ml-4 text-gray-200 font-medium">Rage</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="basis-[55px] bg-main-800"></div>
+            </div>
+            <div class="bg-main-500 grow flex flex-col relative">
+                <slot name="middleSection"/>
+            </div>
+            <div class="bg-main-700 2xl:basis-[315px] basis-[265px] hidden xl:flex xl:flex-col px-5 z-10">
+                <slot name="rightSection"/>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChatLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/views/Chat/IndexView.vue
+++ resources/js/views/Chat/IndexView.vue
@@ -1,218 +1,183 @@
 <template>
-    <div class="bg-main-900 text-white flex flex-col h-screen">
-        <header class="basis-[36px] flex content-end">
-            <div class="text-lg mt-1.5 ml-2.5">reborn</div>
-        </header>
-        <main class="flex bg-main-900 grow">
-            <div class="bg-main-900 basis-[75px]"></div>
-            <div class="bg-main-700 2xl:basis-[315px] basis-[265px] rounded-tl-2xl flex flex-col">
-                <div class="grow flex flex-col items-center mt-6">
-                    <div class="w-full px-5">
-                        <input type="text"
-                               class="bg-main-500 w-full h-8 rounded-[5px] px-2 focus:outline focus:outline-2 focus:outline-slate-600"
-                               placeholder="Search">
-                    </div>
-                    <div class="pt-8 flex justify-between w-full px-5 items-center">
-                        <div>PRIVATE MESSAGES</div>
-                        <div class="text-2xl hover:cursor-pointer">+</div>
-                    </div>
-                    <div class="w-full px-5 pt-5 flex flex-col space-y-4">
-                        <div
-                            class="w-full relative flex items-center after:hidden hover:after:block hover:after:content-['✕'] after:absolute after:top-1 after:right-1 after:text-gray-200 after:cursor-pointer">
-
-                            <div class="bg-white w-8 h-8 rounded-full bg-gray-200"></div>
-                            <div class="ml-4 text-gray-200 font-medium">Schaste</div>
-                        </div>
-                        <div
-                            class="w-full relative flex items-center after:hidden hover:after:block hover:after:content-['✕'] after:absolute after:top-1 after:right-1 after:text-gray-200 after:cursor-pointer">
-
-                            <div class="bg-white w-8 h-8 rounded-full bg-gray-200"></div>
-                            <div class="ml-4 text-gray-200 font-medium">Rage</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="basis-[55px] bg-main-800"></div>
+    <ChatLayout>
+        <template #middleSection>
+            <div :class="$style['chat-block-header']" class="border-b border-main-900 h-12 absolute inset-x-0 top-0 bg-main-500">
+                his
             </div>
-            <div class="bg-main-500 grow flex flex-col px-2 pt-6">
-                <div
-                    class="flex max-h-[835px] grow flex-col-reverse py-4 space-y-2 space-y-reverse overflow-auto  pl-4 pr-2 2xl:px-8 ">
-                    <div class="flex">
+            <div class="px-2">
+                <ul
+                    :class="$style['chat-block']"
+                    class="chat-block flex grow flex-col-reverse space-y-2 space-y-reverse overflow-auto px-2 2xl:px-4 pt-[60px]">
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
+                    </li>
+                    <li class="flex">
                         <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
                         <div class="ml-2">
                             <div class="text-md">Schaste</div>
                             <div class="text-sm">Погнали, сколько можно</div>
                         </div>
-                    </div>
-                    <div class="flex">
-                        <div class="bg-white w-[44px] h-[44px] rounded-full bg-gray-200"></div>
-                        <div class="ml-2">
-                            <div class="text-md">Schaste</div>
-                            <div class="text-sm">Погнали, сколько можно</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="basis-10 py-5 w-full flex pl-4 pr-2 2xl:pl-8 ">
+                    </li>
+                </ul>
+                <div class="basis-10 py-5 w-full flex px-2 2xl:px-4 ">
             <textarea tabindex="0" class="w-full h-auto bg-main-300 rounded-[5px] outline-none px-3 h-[36px] max-h-[200px] pt-1.5 resize-none border-0 focus:ring-0 focus-visible:ring-0 overflow-y-hidden
             " rows="1"></textarea>
                     <div
@@ -220,35 +185,58 @@
                         Send
                     </div>
                 </div>
-
             </div>
-            <div class="bg-main-700 2xl:basis-[315px] basis-[265px] hidden xl:flex xl:flex-col px-5 ">
-                <div class="bg-white w-24 h-24 rounded-full bg-gray-200 mt-10"></div>
-                <div class="mt-4 bg-main-800 w-full rounded-[5px] p-3">
-                    <div class="border-b pb-1 border-main-700">
-                        <div class="text-xl">Meat enjoyer</div>
-                        <div class="text-xs">@steave_12312</div>
-                    </div>
-                    <div class="pt-2.5">
-                        <div class="text-sm font-semibold">ABOUT</div>
-                        <div class="text-xs">Just boy who adore drink so wine time at time</div>
-                    </div>
-                    <div class="pt-2.5">
-                        <div class="text-sm font-semibold">JOINED AT</div>
-                        <div class="text-xs">february 15, 2003</div>
-                    </div>
+
+        </template>
+
+        <template #rightSection>
+            <div class="bg-white w-24 h-24 rounded-full bg-gray-200 mt-10"></div>
+            <div class="mt-4 bg-main-800 w-full rounded-[5px] p-3">
+                <div class="border-b pb-1 border-main-700">
+                    <div class="text-xl">Meat enjoyer</div>
+                    <div class="text-xs">@steave_12312</div>
+                </div>
+                <div class="pt-2.5">
+                    <div class="text-sm font-semibold">ABOUT</div>
+                    <div class="text-xs">Just boy who adore drink so wine time at time</div>
+                </div>
+                <div class="pt-2.5">
+                    <div class="text-sm font-semibold">JOINED AT</div>
+                    <div class="text-xs">february 15, 2003</div>
                 </div>
             </div>
-        </main>
-    </div>
+        </template>
+
+
+    </ChatLayout>
 </template>
 
 <script>
+import ChatLayout from "@/components/Layouts/ChatLayout.vue";
+
 export default {
-    name: "IndexView"
+    name: "IndexView",
+    components: {ChatLayout}
 }
 </script>
 
-<style scoped>
+<style module lang="scss">
+.chat-block {
+    max-height: calc(100vh - 110px);
 
+    &::-webkit-scrollbar {
+        width: 7px;
+        background-color: #2b2d32;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #222427;
+        border-radius: 10px;
+    }
+}
+.chat-block-header{
+    z-index: 1;
+    box-shadow: 0 4px 0.3em rgba(0, 0, 0, 0.46);
+}
 </style>
