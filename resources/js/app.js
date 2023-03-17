b4e691ba7fcfa2a414a796ee36fe087e268c7f6f
Kostya <koooooostaaa@gmail.com>
2023-03-17 21:25:26 +0200

    import font-awesome component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,15 @@ import './../css/app.css'
 import App from './App.vue'
 import router from "@/router"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons'
+
 const app = createApp({})
 
+library.add(faUserGroup)
+
 app.component('App', App)
-app.use(router)
-app.mount('#app')
+.component('font-awesome-icon', FontAwesomeIcon)
+.use(router)
+.mount('#app')
