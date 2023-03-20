706a36a64c9d67cf351087c614581f393b24065b
Kostya <koooooostaaa@gmail.com>
2023-03-20 21:46:17 +0200

    Remove icon to a separate file

--- resources/js/components/Layouts/AuthLayout.vue
+++ resources/js/components/Layouts/AuthLayout.vue
@@ -1,15 +1,14 @@
 <template>
         <section class="bg-gray-50 dark:bg-main-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-                <router-link :to="{ name: 'home'}" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                         alt="logo">
+                <router-link :to="{ name: 'home'}" class="flex items-center mb-6 text-2xl font-semibold text-white">
+                    <LogoIcon></LogoIcon>
                     Reborn
                 </router-link>
                 <div
-                    class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-main-500 dark:border-main-700">
+                    class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-main-500 border-main-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                             {{ title }}
                         </h1>
                         <slot/>
@@ -20,8 +19,11 @@
 </template>
 
 <script>
+import LogoIcon from "@/components/Icons/LogoIcon.vue";
+
 export default {
     name: "AuthLayout",
+    components: {LogoIcon},
     props:{
         title: {
             type: String,
