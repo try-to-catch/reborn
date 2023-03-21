8f4f57fdfa6d8d1fd5677ceecafc9bd41cd2cd01
Kostya <koooooostaaa@gmail.com>
2023-03-21 20:49:24 +0200

    edit db request

--- app/Http/Controllers/API/User/IndexController.php
+++ app/Http/Controllers/API/User/IndexController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\API\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\API\UserResource;
-use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
@@ -15,8 +15,30 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = User::query()->select(['id', 'name', 'username', 'email', 'bio', 'thumbnail', 'created_at'])->with('chats')->find($request->user()->id);
-//        return ['data' => $user];
+        $user = User::query()
+            ->select(['id', 'name', 'username', 'email', 'bio', 'thumbnail', 'created_at'])
+            ->with(['chats' => function ($query) {
+                $query->whereHas('messages')
+                    ->select(['chats.id'])
+                    ->selectSub(function ($query) {
+                        $query->from('messages')
+                            ->whereColumn('messages.chat_id', 'chats.id')
+                            ->selectRaw('MAX(created_at)');
+                    }, 'last_message_date')
+                    ->selectSub(function ($query) {
+                        $query->from('users')
+                            ->whereColumn('chats.friend_id', 'users.id')
+                            ->selectRaw('users.name');
+                    }, 'friend_name')
+                    ->selectSub(function ($query) {
+                        $query->from('users')
+                            ->whereColumn('chats.friend_id', 'users.id')
+                            ->selectRaw('users.thumbnail');
+                    }, 'friend_thumbnail')
+                    ->orderByDesc('last_message_date');
+
+            }])
+            ->find($request->user()->id);
         return new UserResource($user);
     }
 }
