6f583fa40cb64c5441c5546764a4e4301a71cf94
Kostya <koooooostaaa@gmail.com>
2023-03-24 20:50:50 +0200

    create ShowController

---
+++ app/Http/Controllers/API/Chat/ShowController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API\Chat;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\API\Chat\ChatResource;
+use App\Models\Chat;
+
+class ShowController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Chat $chat): ChatResource
+    {
+        $chatData = $chat->select('id')->with(['messages', 'users' => function ($query) {
+            $query->where('users.id', '!=', request()->user()->id);
+        }])->find($chat->id);
+
+        return new ChatResource($chatData);
+    }
+}
