784bb53b57e1881535c1afc385011d104330089b
Kostya <koooooostaaa@gmail.com>
2023-03-24 20:54:45 +0200

    remove unusable field from fillable

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,7 +10,7 @@ class Message extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'chat_id', 'created_at', 'text'];
+    protected $fillable = ['user_id', 'chat_id', 'text'];
 
     public function chat(): BelongsTo
     {
