243b8a39eb4625fe427a3f07f26eb748ebd0fe7b
Kostya <koooooostaaa@gmail.com>
2023-03-26 18:34:35 +0300

    add createMessage function, and fix small bugs

--- resources/js/composables/chat.js
+++ resources/js/composables/chat.js
@@ -6,6 +6,7 @@ export default function useChat() {
     const route = useRoute()
 
     const isLoading = ref(false)
+    const pendingMessages = ref([])
     const chat = reactive({})
 
     onMounted(setChat)
@@ -13,25 +14,39 @@ export default function useChat() {
 
     onBeforeRouteUpdate(async (to, from) => {
         if (to.params.id !== from.params.id) {
-            await setChat()
+            await setChat(to.params.id)
         }
     })
 
 
     async function getChat(chatId) {
-        return axios.post(`/api/chats/${chatId}`).then(res => res.data.data)
+        return axios.get(`/api/chats/${chatId}`).then(res => res.data.data)
     }
 
 
-    async function setChat() {
+    async function setChat(chatId = route.params.id) {
         isLoading.value = true;
 
-        await getChat(route.params.id).then(res => {
+        await getChat(chatId).then(res => {
             Object.assign(chat, res);
         })
 
         isLoading.value = false
     }
 
-    return {isLoading, chat}
+
+    async function createMessage(text) {
+        pendingMessages.value.push(text)
+        await axios.post(`/api/messages/${chat.id}`, {text}).then((res) => {
+            const deliveredMessage = res.data.data
+
+            chat.messages.push(deliveredMessage)
+
+            pendingMessages.value = pendingMessages.value.filter((message) => {
+                return message !== deliveredMessage.text
+            })
+        })
+    }
+
+    return {isLoading, chat, createMessage, pendingMessages}
 }
