e33bffe4209c52aab93aff890d97c5875a7f4169
Kostya <koooooostaaa@gmail.com>
2023-03-27 21:46:29 +0300

    add broadcast event dispatcher

--- app/Http/Controllers/API/Message/StoreController.php
+++ app/Http/Controllers/API/Message/StoreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API\Message;
 
+use App\Events\MessageSent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Message\StoreFormRequest;
 use App\Http\Resources\API\Message\MessageResource;
@@ -12,12 +13,13 @@ class StoreController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Chat $chat, StoreFormRequest $request)
+    public function __invoke(Chat $chat, StoreFormRequest $request): MessageResource
     {
         $message = $chat->messages()->create([
             'user_id' => $request->user()->id,
             'text' => $request->validated()['text']
         ]);
+        broadcast(new MessageSent($chat, $message));
 
         return new MessageResource($message);
     }
