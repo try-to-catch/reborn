0d2d5fe4f42f128367a9d8708508c87018ce3ea1
Kostya <koooooostaaa@gmail.com>
2023-03-28 20:28:53 +0300

    create Chat/IndexController

---
+++ app/Http/Controllers/API/Chat/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API\Chat;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\API\Chat\ChatMinCollection;
+use App\Models\Chat;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $chats = request()->user()->chats()->whereHas('messages')->select(['chats.id'])
+            ->selectSub(function ($query) {
+                $query->from('messages')
+                    ->whereColumn('messages.chat_id', 'chats.id')
+                    ->selectRaw('MAX(created_at)');
+            }, 'last_message_date')
+            ->with(['users' => function ($query) {
+                $query->where('users.id', '!=', request()->user()->id);
+            }])
+            ->orderByDesc('last_message_date')->get();
+
+        return new ChatMinCollection($chats);
+    }
+}
