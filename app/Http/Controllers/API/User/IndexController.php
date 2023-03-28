811b696f9d1de0778bf8aca5977d148407452459
Kostya <koooooostaaa@gmail.com>
2023-03-28 20:30:30 +0300

    make controller single responsibility

--- app/Http/Controllers/API/User/IndexController.php
+++ app/Http/Controllers/API/User/IndexController.php
@@ -16,27 +16,6 @@ class IndexController extends Controller
     {
         $user = User::query()
             ->select(['id', 'name', 'username', 'email', 'bio', 'thumbnail', 'created_at'])
-            ->with(['chats' => function ($query) {
-                $query->whereHas('messages')
-                    ->select(['chats.id'])
-                    ->selectSub(function ($query) {
-                        $query->from('messages')
-                            ->whereColumn('messages.chat_id', 'chats.id')
-                            ->selectRaw('MAX(created_at)');
-                    }, 'last_message_date')
-                    ->selectSub(function ($query) {
-                        $query->from('users')
-                            ->whereColumn('chats.friend_id', 'users.id')
-                            ->selectRaw('users.name');
-                    }, 'friend_name')
-                    ->selectSub(function ($query) {
-                        $query->from('users')
-                            ->whereColumn('chats.friend_id', 'users.id')
-                            ->selectRaw('users.thumbnail');
-                    }, 'friend_thumbnail')
-                    ->orderByDesc('last_message_date');
-
-            }])
             ->find($request->user()->id);
         return new UserResource($user);
     }
