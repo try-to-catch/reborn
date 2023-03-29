ddfb057a6ac13dc6ac1d5c4abfc306cd190656cd
Kostya <koooooostaaa@gmail.com>
2023-03-29 21:25:51 +0300

    create model

---
+++ app/Models/ChatRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ChatRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'friend_id', 'was_considered'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function friend(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
