26025393252a897720daf89fe9d2123614deca0c
Kostya <koooooostaaa@gmail.com>
2023-03-31 22:49:25 +0300

    edit namespace

--- app/Http/Requests/API/ChatRequest/StoreFormRequest.php
+++ app/Http/Requests/API/ChatRequest/StoreFormRequest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Requests\API\Chat;
+namespace App\Http\Requests\API\ChatRequest;
 
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class ChatFormRequest extends FormRequest
+class StoreFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
