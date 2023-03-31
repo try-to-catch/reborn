7bdaaa601f3b5f3c4f5fc95bdfdcdd5ef0e225c6
Kostya <koooooostaaa@gmail.com>
2023-03-31 22:48:56 +0300

    make composable more clear and add some methods

--- resources/js/composables/friend.js
+++ resources/js/composables/friend.js
@@ -1,24 +1,61 @@
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
 export default function useFriend() {
-
+    const isLoading = ref(false)
     const searchData = ref([])
 
-    async function friendSearch(username) {
-        console.log({username})
-        return await axios.post('/api/users/search', {username}).then(r => {
-            console.log(r)
-            searchData.value = [...r.data.data]
-        }).catch((e) => {
-            console.log(e)
-        })
+    function withLoading(fn) {
+        return async (...args) => {
+            isLoading.value = true
+            try {
+                return await fn(...args)
+            } finally {
+                isLoading.value = false
+            }
+        }
     }
 
-    async function sendChatRequest(userId){
-        return await axios.post('/api/chats/chat-request', {id: userId}).then(r => {
-            console.log(r)
+
+    const friendSearch = withLoading(async function (username) {
+        return await axios.post('/api/users/search', {username}).then((res) => {
+            return searchData.value = [...res.data.data]
         })
+    })
+
+
+    const sentRequests = reactive([])
+
+    const sendChatRequest = withLoading(async function (userId) {
+        sentRequests.push(userId)
+
+        return await axios.post('/api/chats/chat-requests', {id: userId})
+    })
+
+
+    const chatRequests = reactive([])
+
+    const getChatRequests = withLoading(async function () {
+        return await axios.get('/api/chats/chat-requests').then(res => {
+            return chatRequests.push(...res.data.data)
+        })
+    })
+
+    const considerRequest = async function (requestId, sender_id, is_accept) {
+        return await axios.patch(`/api/chats/chat-requests/${requestId}`, {sender_id, is_accept})
+            .then(r => {
+                console.log(r)
+                return r
+            })
     }
 
-    return {friendSearch, searchData, sendChatRequest}
+    return {
+        friendSearch,
+        searchData,
+        sendChatRequest,
+        getChatRequests,
+        isLoading,
+        chatRequests,
+        considerRequest,
+        sentRequests
+    }
 }
