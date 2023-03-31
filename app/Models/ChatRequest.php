ff112175c317c5c8e3bce0d684e680db02989eb9
Kostya <koooooostaaa@gmail.com>
2023-03-31 22:49:43 +0300

    edit relationship

--- app/Models/ChatRequest.php
+++ app/Models/ChatRequest.php
@@ -10,14 +10,14 @@ class ChatRequest extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'friend_id', 'was_considered'];
+    protected $fillable = ['user_id', 'sender_id', 'was_considered'];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function friend(): BelongsTo
+    public function sender(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
