583ca7295e6c6c62c207c5040c019ff9af421536
Kostya <koooooostaaa@gmail.com>
2023-03-31 22:50:09 +0300

    add some icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,11 +6,11 @@ import router from "@/router"
 
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {faAt, faGear, faMagnifyingGlass, faSquareCheck, faUserGroup} from '@fortawesome/free-solid-svg-icons'
+import {faAt, faGear, faMagnifyingGlass, faSquareCheck, faUserGroup, faCheck, faXmark} from '@fortawesome/free-solid-svg-icons'
 
 const app = createApp({})
 
-library.add(faUserGroup, faGear, faAt, faMagnifyingGlass, faSquareCheck)
+library.add(faUserGroup, faGear, faAt, faMagnifyingGlass, faSquareCheck, faCheck, faXmark)
 
 app.component('App', App)
     .component('font-awesome-icon', FontAwesomeIcon)
