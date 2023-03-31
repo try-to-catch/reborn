eaa2e4332c4e5227183057c74368c28dd4ea90b9
Kostya <koooooostaaa@gmail.com>
2023-03-31 22:45:36 +0300

    create UpdateFormRequest

---
+++ app/Http/Requests/API/ChatRequest/UpdateFormRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\API\ChatRequest;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sender_id' => ['required', 'integer', 'exists:users,id'],
+            'is_accept' => ['required', 'boolean']
+        ];
+    }
+}
