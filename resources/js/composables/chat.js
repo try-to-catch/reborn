0bb194f3298218f4f47991e22ed365883d86c4c5
Kostya <koooooostaaa@gmail.com>
2023-04-01 21:25:53 +0300

    add deleteChat method, change chats type (from reactive to ref)

--- resources/js/composables/chat.js
+++ resources/js/composables/chat.js
@@ -6,25 +6,23 @@ export default function useChat() {
     const route = useRoute()
 
     const isLoading = ref(false)
-
     onBeforeRouteUpdate(async (to, from) => {
         if (to.params.id !== from.params.id) {
             await setChat(to.params.id)
         }
     })
 
-
-    const chats = reactive([])
+    const chats = ref([])
 
     async function getChats() {
         return await axios.get('/api/chats').then(res => res.data.data)
     }
 
     async function setChats() {
-        isLoading.value = true;
+        isLoading.value = true
 
         await getChats().then(res => {
-            chats.push(...res)
+            chats.value.push(...res)
         })
 
         isLoading.value = false
@@ -39,10 +37,10 @@ export default function useChat() {
 
 
     async function setChat(chatId = route.params.id) {
-        isLoading.value = true;
+        isLoading.value = true
 
         await getChat(chatId).then(res => {
-            Object.assign(chat, res);
+            Object.assign(chat, res)
         })
 
         isLoading.value = false
@@ -62,14 +60,25 @@ export default function useChat() {
         })
     }
 
+
+    async function deleteChat(chatId) {
+        return await axios.delete(`/api/chats/${chatId}`).then(() => {
+            chats.value = chats.value.filter((item) => {
+                return item.id !== chatId
+            })
+        })
+    }
+
+
     const isAuthenticated = localStorage.getItem('x_xsrf_token')
+
     watchEffect(() => {
         if (chat.id && isAuthenticated) {
             Echo.private(`chats.${chat.id}`)
                 .listen('MessageSent', (e) => {
                     chat.messages.push(e.message)
-                });
+                })
         }
     })
-    return {isLoading, chat,setChat, createMessage, pendingMessages, chats, setChats}
+    return {isLoading, chat, setChat, createMessage, pendingMessages, chats, setChats, deleteChat}
 }
