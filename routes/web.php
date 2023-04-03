65ba5f364937c34c578f84db8c1dcd14d40260e4
Kostya <koooooostaaa@gmail.com>
2023-04-03 20:48:15 +0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', \App\Http\Controllers\Auth\LoginController::class);
-Route::post('/sign-up', \App\Http\Controllers\Auth\RegisterController::class);
+Route::post('/login', \App\Http\Controllers\Auth\LoginController::class)->middleware('guest');
+Route::post('/sign-up', \App\Http\Controllers\Auth\RegisterController::class)->middleware('guest');
+Route::post('/forgot-password', \App\Http\Controllers\Auth\ForgotPasswordController::class)->middleware('guest');
+Route::post('/reset-password', \App\Http\Controllers\Auth\ResetPasswordController::class)->middleware('guest');
 
 Route::view('/{page}', 'app')->where('page', '.*');
