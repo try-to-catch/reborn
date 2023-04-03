548ac0c6188f92476d4ebf864ead7ef5791d8bdc
Kostya <koooooostaaa@gmail.com>
2023-04-03 20:59:50 +0300

    customize password reset link

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,8 +22,10 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot(Request $request): void
     {
-        //
+        ResetPassword::createUrlUsing(function (User $user, string $token) use ($request) {
+            return env('APP_URL').'reset-password/'.$token.'?'.http_build_query(['email' => $request->email]);
+        });
     }
 }
