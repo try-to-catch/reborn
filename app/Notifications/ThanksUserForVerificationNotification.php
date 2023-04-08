f166052683d1db7701f0b39158b7eea80e1bc9ef
Kostya <koooooostaaa@gmail.com>
2023-04-08 15:48:26 +0300

    create notification

---
+++ app/Notifications/ThanksUserForVerificationNotification.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ThanksUserForVerificationNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public readonly User $user
+    )
+    {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->greeting('Hello ' . $this->user->name . '!')
+            ->line('Thank you for verification! From now on, all reborn features are available for you, enjoy!')
+            ->action('Go to home', url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
