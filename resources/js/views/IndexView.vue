e68084821713d35657c88dd34110c155f02c27db
Kostya <koooooostaaa@gmail.com>
2023-04-08 15:52:47 +0300

    add the ability to logout

--- resources/js/views/IndexView.vue
+++ resources/js/views/IndexView.vue
@@ -2,18 +2,26 @@
     <div>
         Index View
         <div class="space-x-2 text-blue-700 mt-10">
-            <router-link :to="{ name: 'login'}">Login</router-link>
-            <router-link :to="{ name: 'chats.index'}">Chats</router-link>
+            <template v-if="!isAuthenticated">
+                <router-link :to="{ name: 'login'}">Login</router-link>
+            </template>
+            <template v-else>
+                <router-link :to="{ name: 'chats.index'}">Chats</router-link>
+                <a @click.prevent="logout" href="/logout">Logout</a>
+            </template>
         </div>
     </div>
 </template>
 
 <script>
+import useUser from "@/composables/user";
+
 export default {
-    name: "IndexView"
+    name: "IndexView",
+    setup() {
+        const {isAuthenticated, logout} = useUser()
+
+        return {isAuthenticated, logout}
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
