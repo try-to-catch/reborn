45990bdbfa09020aca185a73574e98d19fc7de23
Kostya <koooooostaaa@gmail.com>
2023-04-08 15:57:47 +0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', \App\Http\Controllers\Auth\LoginController::class)->middleware('guest');
-Route::post('/sign-up', \App\Http\Controllers\Auth\RegisterController::class)->middleware('guest');
-Route::post('/forgot-password', \App\Http\Controllers\Auth\ForgotPasswordController::class)->middleware('guest');
-Route::post('/reset-password', \App\Http\Controllers\Auth\ResetPasswordController::class)->middleware('guest');
+Route::post('/login', LoginController::class)->middleware('guest');
+Route::post('/sign-up', RegisterController::class)->middleware('guest');
+Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+Route::post('/forgot-password', ForgotPasswordController::class)->middleware('guest');
+Route::post('/reset-password', ResetPasswordController::class)->middleware('guest');
+
+Route::get('/email/verify/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed'])
+    ->name('verification.verify');
 
 Route::view('/{page}', 'app')->where('page', '.*');
